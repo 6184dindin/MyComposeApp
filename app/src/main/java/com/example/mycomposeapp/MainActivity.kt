6797793b3facc345af38ae7b9f4bf0817f77bb7c
dientv <dientv@rikkeisoft.com>
2023-07-17 17:35:56 +0700

    commit

--- app/src/main/java/com/example/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapp/MainActivity.kt
@@ -1,13 +1,22 @@
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class,
+    ExperimentalComposeUiApi::class
+)
+
 package com.example.mycomposeapp
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,32 +24,45 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.internal.enableLiveLiterals
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
@@ -51,6 +73,11 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
@@ -58,6 +85,8 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.example.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -70,6 +99,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    HomeScreen()
                 }
             }
         }
@@ -85,7 +115,257 @@ fun HomeScreen() {
 //        ClickableSample()
 //        DetectTapGesturesSample()
 //        CheckboxSample()
+//        TextFieldSample()
+//        OutlineTextFieldSample()
+//        RowSample()
+//        ConstraintLayoutSample()
     }
+}
+
+//need to add this in build.gradle (app)
+@Composable
+fun ConstraintLayoutSample() {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+    ) {
+        val (redBox, greenBox, blueBox) = createRefs()
+        Box(
+            modifier = Modifier
+                .size(100.dp, 100.dp)
+                .background(Color.Red)
+                .constrainAs(redBox) {
+                    top.linkTo(parent.top, margin = 16.dp)
+                    start.linkTo(parent.start, margin = 16.dp)
+                }
+        )
+        Box(
+            modifier = Modifier
+                .size(100.dp, 100.dp)
+                .background(Color.Green)
+                .constrainAs(greenBox) {
+                    top.linkTo(redBox.bottom, margin = 16.dp)
+                    start.linkTo(redBox.end, margin = 16.dp)
+                }
+        )
+        Box(
+            modifier = Modifier
+                .size(100.dp, 100.dp)
+                .background(Color.Blue)
+                .constrainAs(blueBox) {
+                    top.linkTo(greenBox.bottom, margin = 16.dp)
+                    start.linkTo(greenBox.end, margin = 16.dp)
+                }
+        )
+
+        val guideline = createGuidelineFromTop(fraction = 0.5f)
+        val yellowBox = createRef()
+        Box(
+            modifier = Modifier
+                .size(100.dp, 100.dp)
+                .background(Color.Yellow)
+                .constrainAs(yellowBox) {
+                    top.linkTo(guideline)
+                    start.linkTo(parent.start)
+                }
+        )
+
+        val barrier = createBottomBarrier(blueBox, yellowBox)
+        val blackBox = createRef()
+        Box(
+            modifier = Modifier
+                .background(Color.Black)
+                .constrainAs(blackBox) {
+                    top.linkTo(barrier)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    width = Dimension.fillToConstraints
+                }
+                .aspectRatio(3f)
+        )
+    }
+}
+
+@Composable
+fun RowSample() {
+    Box(
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly, // horizontal: start, end, center, spaceBetween, spaceEvenly, spaceAround
+            modifier = Modifier
+                .size(350.dp, 150.dp)
+                .background(Color.Gray),
+            verticalAlignment = Alignment.Top,
+        ) {
+            ItemBox(Color.Red)
+            ItemBox(Color.Green)
+            ItemBox(Color.Blue)
+        }
+    }
+    Box(
+    ) {
+        Column(
+            modifier = Modifier
+                .size(350.dp, 300.dp)
+                .background(Color.Gray),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly // vertical: top, bottom, center, spaceBetween, spaceEvenly, spaceAround
+        ) {
+            ItemBox(Color.Red)
+            ItemBox(Color.Green)
+            ItemBox(Color.Blue)
+        }
+    }
+    Box(
+    ) {
+        Row(
+            modifier = Modifier
+                .size(350.dp, 150.dp)
+                .background(Color.Gray)
+                .horizontalScroll(rememberScrollState()),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            ItemBox(Color.Red)
+            ItemBox(Color.Green)
+            ItemBox(Color.Blue)
+            ItemBox(Color.Red)
+            ItemBox(Color.Green)
+            ItemBox(Color.Blue)
+            ItemBox(Color.Red)
+            ItemBox(Color.Green)
+            ItemBox(Color.Blue)
+            ItemBox(Color.Red)
+            ItemBox(Color.Green)
+            ItemBox(Color.Blue)
+        }
+    }
+}
+
+@Composable
+fun ItemBox(color: Color) {
+    Box(
+        Modifier
+            .size(50.dp, 50.dp)
+            .background(color)
+    )
+}
+
+@Composable
+fun TextFieldSample() {
+    var text by remember {
+        mutableStateOf("")
+    }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = {
+            Text(text = "Name")
+        },
+        placeholder = {
+            Text(text = "Enter your name")
+        },
+        leadingIcon = {
+            Icon(Icons.Default.AccountBox, contentDescription = "")
+        },
+        trailingIcon = {
+            IconButton(onClick = {
+                text = ""
+            }) {
+                Icon(Icons.Default.Clear, contentDescription = "")
+            }
+        },
+        isError = text.length < 5,
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            errorIndicatorColor = Color.Transparent,
+            disabledTextColor = Color.Red,
+            focusedLabelColor = Color.Red,
+            unfocusedLabelColor = Color.Red,
+            disabledLabelColor = Color.Red,
+            errorLabelColor = Color.Red,
+            cursorColor = Color.Red,
+        ),
+        shape = RoundedCornerShape(10.dp),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done, // Done, Search, Go, Send, Next, Previous
+            keyboardType = KeyboardType.Text, // Text, Number, Email, Password, Phone, Uri
+            capitalization = KeyboardCapitalization.Words, // None, Characters, Words, Sentences
+        ),
+        keyboardActions = KeyboardActions( // Done, Search, Go, Send, Next, Previous
+            onDone = {},
+            onGo = {},
+            onSearch = {},
+            onSend = {},
+            onNext = {},
+            onPrevious = {}
+        )
+    )
+}
+
+@Composable
+fun OutlineTextFieldSample() {
+    var email by remember {
+        mutableStateOf("")
+    }
+    OutlinedTextField(
+        value = email,
+        onValueChange = {
+            email = it
+        },
+        label = {
+            Text(text = "Email")
+        },
+        placeholder = {
+            Text(text = "Enter your email")
+        },
+        leadingIcon = {
+            Icon(Icons.Default.Email, contentDescription = "")
+        },
+    )
+
+    Space()
+
+    var password by remember {
+        mutableStateOf("")
+    }
+    var passwordVisibility by remember {
+        mutableStateOf(false)
+    }
+    OutlinedTextField(
+        value = password,
+        onValueChange = {
+            password = it
+        },
+        label = {
+            Text(text = "Password")
+        },
+        placeholder = {
+            Text(text = "Enter your password")
+        },
+        trailingIcon = {
+            IconButton(onClick = {
+                passwordVisibility = !passwordVisibility
+            }) {
+                Icon(
+                    if (passwordVisibility) Icons.Default.Done else Icons.Default.Lock,
+                    contentDescription = ""
+                )
+            }
+        },
+        visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done, // Done, Search, Go, Send, Next, Previous
+            keyboardType = KeyboardType.Password, // Text, Number, Email, Password, Phone, Uri
+            capitalization = KeyboardCapitalization.None, // None, Characters, Words, Sentences
+        ),
+    )
 }
 
 @Composable
@@ -101,11 +381,13 @@ fun CheckboxSample() {
             isSelected = !isSelected
         }
     )) {
-        Checkbox(checked = isSelected, onCheckedChange = null,
-        colors = CheckboxDefaults.colors(
-            checkedColor = Color.Red,
-            uncheckedColor = Color.Black
-        ))
+        Checkbox(
+            checked = isSelected, onCheckedChange = null,
+            colors = CheckboxDefaults.colors(
+                checkedColor = Color.Red,
+                uncheckedColor = Color.Black
+            )
+        )
         Text(text = "Select me")
     }
 }
