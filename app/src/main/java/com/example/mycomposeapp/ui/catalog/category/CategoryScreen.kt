a9693f04df0d39093c618de71bc821125ebc7af5
Tô Văn Diện <dientv@rikkeisoft.com>
2023-07-21 10:29:07 +0700

    lifecycle state

--- app/src/main/java/com/example/mycomposeapp/ui/catalog/category/CategoryScreen.kt
+++ app/src/main/java/com/example/mycomposeapp/ui/catalog/category/CategoryScreen.kt
@@ -1,5 +1,7 @@
 package com.example.mycomposeapp.ui.catalog.category
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material3.Button
@@ -7,13 +9,57 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 
 const val CATEGORY_SCREEN_ROUTE = "category_screen_route"
 fun getOpenCategoryScreenRoute() = CATEGORY_SCREEN_ROUTE
 
 @Composable
 fun CategoryScreen(openProductDetail: (String) -> Unit) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    // If `lifecycleOwner` changes, dispose and reset the effect
+    DisposableEffect(lifecycleOwner) {
+        // Create an observer that triggers our remembered callbacks
+        // for lifecycle events
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> {
+                    Log.d("CategoryScreen", "ON_CREATE")
+                }
+                Lifecycle.Event.ON_START -> {
+                    Log.d("CategoryScreen", "ON_START")
+                }
+                Lifecycle.Event.ON_RESUME -> {
+                    Log.d("CategoryScreen", "ON_RESUME")
+                }
+                Lifecycle.Event.ON_PAUSE -> {
+                    Log.d("CategoryScreen", "ON_PAUSE")
+                }
+                Lifecycle.Event.ON_STOP -> {
+                    Log.d("CategoryScreen", "ON_STOP")
+                }
+                Lifecycle.Event.ON_DESTROY -> {
+                    Log.d("CategoryScreen", "ON_DESTROY")
+                }
+                else -> {}
+            }
+        }
+
+        // Add the observer to the lifecycle
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        // When the effect leaves the Composition, remove the observer
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+            Log.d("CategoryScreen", "composition EXIT")
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
--- app/src/main/java/com/example/mycomposeapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/mycomposeapp/ui/home/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.example.mycomposeapp.ui.home
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,9 +10,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 
 const val HOME_SCREEN_ROUTE = "home"
 
@@ -21,6 +28,44 @@ fun HomeScreen(
     editCustomerInfo: () -> Unit,
     openAddressList: () -> Unit
 ) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    // If `lifecycleOwner` changes, dispose and reset the effect
+    DisposableEffect(lifecycleOwner) {
+        // Create an observer that triggers our remembered callbacks
+        // for lifecycle events
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> {
+                    Log.d("HomeScreen", "ON_CREATE")
+                }
+                Lifecycle.Event.ON_START -> {
+                    Log.d("HomeScreen", "ON_START")
+                }
+                Lifecycle.Event.ON_RESUME -> {
+                    Log.d("HomeScreen", "ON_RESUME")
+                }
+                Lifecycle.Event.ON_PAUSE -> {
+                    Log.d("HomeScreen", "ON_PAUSE")
+                }
+                Lifecycle.Event.ON_STOP -> {
+                    Log.d("HomeScreen", "ON_STOP")
+                }
+                Lifecycle.Event.ON_DESTROY -> {
+                    Log.d("HomeScreen", "ON_DESTROY")
+                }
+                else -> {}
+            }
+        }
+
+        // Add the observer to the lifecycle
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        // When the effect leaves the Composition, remove the observer
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+            Log.d("HomeScreen", "composition EXIT")
+        }
+    }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
